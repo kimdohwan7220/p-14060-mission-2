ec8175916cc592aadf520f45f145dffa4f9f423d
kimdohwan7220 <dohwan8220@naver.com>
2025-11-03 11:32:35 +0900

    feat: 명언 등록 기능 구현

---
+++ src/main/java/mission1/domain/QuoteRepository.java
@@ -0,0 +1,23 @@
+package mission1.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuoteRepository {
+    private final List<Quote> quotes = new ArrayList<>();
+    private int nextId = 1; // 자동 증가 ID
+
+    public Quote save(String content, String author) {
+        Quote quote = new Quote(getNextId(), content, author);
+        quotes.add(quote);
+        return quote;
+    }
+
+    private int getNextId() {
+        return nextId++;
+    }
+
+    public List<Quote> findAll() {
+        return new ArrayList<>(quotes);
+    }
+}
