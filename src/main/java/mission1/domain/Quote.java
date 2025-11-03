7b7ad1ea8e1b16a0da1cad6b2b60d1dccd644f59
kimdohwan7220 <dohwan8220@naver.com>
2025-11-03 11:23:06 +0900

    feat: 명언 객체 클래스 추가

---
+++ src/main/java/mission1/domain/Quote.java
@@ -0,0 +1,25 @@
+package mission1.domain;
+
+public class Quote {
+    private final int id;
+    private final String content;
+    private final String author;
+
+    public Quote(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
