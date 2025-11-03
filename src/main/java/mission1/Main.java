7f517790fa9893e22fe8675be78cfe26613bb3cc
kimdohwan7220 <dohwan8220@naver.com>
2025-11-03 11:18:17 +0900

    refactor: 구조 수정

--- src/main/java/mission1/Main.java
+++ src/main/java/mission1/Main.java
@@ -1,11 +1,9 @@
 package mission1;
 
+import mission1.controller.AppController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        new AppController().run();
     }
 }
--- src/test/java/mission1/Main.java
+++
@@ -1,9 +0,0 @@
-package mission1;
-
-import mission1.controller.AppController;
-
-public class Main {
-    public static void main(String[] args) {
-        new AppController().run();
-    }
-}
