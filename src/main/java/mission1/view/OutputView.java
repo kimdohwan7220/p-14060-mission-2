36082c562f09272e7669f46ff7f299b31b3bc2d4
kimdohwan7220 <dohwan8220@naver.com>
2025-11-03 11:51:02 +0900

    refactor: 명언 등록 출력 추가

--- src/main/java/mission1/view/OutputView.java
+++ src/main/java/mission1/view/OutputView.java
@@ -1,5 +1,7 @@
 package mission1.view;
 
+import mission1.domain.Quote;
+
 public class OutputView {
 
     public static void printHeader() {
@@ -8,5 +10,9 @@ public class OutputView {
 
     public static void printExit() {
         System.out.println("프로그램을 종료합니다.");
+    }
+
+    public static void printQuoteRegistered(Quote quote) {
+        System.out.println(quote.getId() + "번 명언이 등록되었습니다.");
     }
 }
